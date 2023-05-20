2e48bb70b3e64a663d338df5fdd78211dc6bbbe7
HP <HP@LAPTOP-JCK5A7JI>
2023-05-20 21:04:55 +0530

    Firstcommit

---
+++ src/com/demo/Test.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+//import com.infy.exception.CarsException;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int spc=0;
+		if(spc==0) {
+			//throw new CarsException("ERROR occcured");
+			System.out.println("SUCCESS");
+		}
+		else {
+System.out.println("ERROR");		}
+	}
+
+}
